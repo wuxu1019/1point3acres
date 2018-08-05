c0e8ac7dea92e5e97c04e7649b0fc5dba1741e0e
wuxu <wuxu1019@hotmail.com>
2018-08-05 15:29:28 -0700

    thre solution

--- Google/test_681_Next_Closest_Time.py
+++ Google/test_681_Next_Closest_Time.py
@@ -83,3 +83,11 @@ class Solution(object):
                     ans = cur
 
         return '{0:02d}:{1:02d}'.format(*divmod(ans, 60))
+
+
+    def nextClosestTime_3(self, time):
+
+        return min((t <= time, t)
+                   for i in range(24 * 60)
+                   for t in ['%02d:%02d' % divmod(i, 60)]
+                   if set(t) <= set(time))[1]
